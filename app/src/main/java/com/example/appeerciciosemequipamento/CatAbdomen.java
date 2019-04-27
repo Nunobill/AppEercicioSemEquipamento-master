c26519b5e02166bb654e63370158509167716ad1
Nuno Bill Afonso da Silva <1012377@sal.ipg.pt>
2019-04-27 18:47:55 +0100

    gets e sets da primeira categoria

--- app/src/main/java/com/example/appeerciciosemequipamento/CatAbdomen.java
+++ app/src/main/java/com/example/appeerciciosemequipamento/CatAbdomen.java
@@ -1,4 +1,30 @@
 package com.example.appeerciciosemequipamento;
 
 public class CatAbdomen {
+    int id_abdomen;
+    String Categoria_Ab;
+
+
+
+    public CatAbdomen (int id_abdomen, String Categoria_Ab ){
+        this.id_abdomen = id_abdomen ;
+        this.Categoria_Ab= Categoria_Ab;
+    }
+
+    //Setters and getters
+    public int getId_abdomen() {
+        return id_abdomen;
+    }
+
+    public void setId_abdomen(int id_abdomen) {
+        this.id_abdomen = id_abdomen;
+    }
+
+    public String getCategoria_Ab() {
+        return Categoria_Ab;
+    }
+
+    public void setCategoria_Ab(String categoria_Ab) {
+        Categoria_Ab = categoria_Ab;
+    }
 }
