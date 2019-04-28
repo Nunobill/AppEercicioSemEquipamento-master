ba67a0720006c839095afcdd295b9b87a90822f4
Nuno Bill Afonso da Silva <1012377@sal.ipg.pt>
2019-04-28 11:12:54 +0100

    criação do layout Eliminar da segunda categoria

---
+++ app/src/main/java/com/example/appeerciciosemequipamento/Categoria2Activity.java
@@ -0,0 +1,53 @@
+package com.example.appeerciciosemequipamento;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class Categoria2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categoria2);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    public void Escolher2(View view) {
+        Toast.makeText(this, "Escolher Categorias...", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this, EscolherActivity.class);
+        startActivity(intent);
+    }
+
+    public void Adicionar2(View view) {
+        Toast.makeText(this, "Adicionar Categorias...", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this, AdicionarActivity.class);
+
+        startActivity(intent);
+    }
+
+
+    public void Editar2(View view) {
+        Toast.makeText(this, "Editar Categorias...", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this, EditarActivity.class);
+        startActivity(intent);
+    }
+
+    public void Eliminar2(View view) {
+        Toast.makeText(this, "Eliminar Categorias...", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this, EliminarActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/appeerciciosemequipamento/Eliminar1Activity.java
@@ -0,0 +1,30 @@
+package com.example.appeerciciosemequipamento;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class Eliminar1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eliminar1);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void CancelarElimPern(View view) {
+        finish();
+    }
+
+    public void eliminar1Pern(View view) {
+        Toast.makeText(this, "Eliminado com sucesso!...", Toast.LENGTH_SHORT).show();
+    }
+}
